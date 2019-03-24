861b5f23f376a1cfcca8949d42ec91eb43cd9608
Martin Korinek <work@korinek.cc>
2019-03-24 21:07:14 +0100

    Add final sum to result

--- monthly_rate.py
+++ monthly_rate.py
@@ -5,9 +5,9 @@
 import sys, argparse
     
 parser = argparse.ArgumentParser(description="Loan Payment Calculator")
-parser.add_argument("-p",   "--principal", type=float, default=100000, help="Principal Loan")
+parser.add_argument("-p",   "--principal", type=float, default=106000, help="Principal Loan")
 parser.add_argument("-i",   "--interest", type=float, required=True, help="Annual interest rate")
-parser.add_argument("-n",   "--number_of_months", type=int, default=360, help="Count of payments")
+parser.add_argument("-n",   "--number_of_months", type=int, default=240, help="Count of payments")
 parser.add_argument("-v",   "--verbosity", action="count", default=0, help="Show monthly balance")
 args = parser.parse_args()
 verboseMode = args.verbosity
@@ -18,7 +18,8 @@ LoanTerm = args.number_of_months
 irate = ((1+MortgageAPR/100.0) ** (1.0/12)) - 1
 MonthlyPmnt = PrimaryLoan * (irate * ((1+irate) ** LoanTerm)) / (((1+irate) ** LoanTerm) - 1)
 print ("Monthly payment is", '{:.2f}'.format(MonthlyPmnt), "for loan of", PrimaryLoan, \
-                "paid back in", LoanTerm, "months at a fixed APR of", '{:.4f}'.format(MortgageAPR))
+                "paid back in", LoanTerm, "months at a fixed APR of", '{:.4f}'.format(MortgageAPR), \
+                    'results in a sum of ','{:.2f}'.format(LoanTerm*MonthlyPmnt))
     
 if verboseMode == 0:
     print ("Use the -v option for a list of monthly loan balance and equity over these", LoanTerm, "months")
